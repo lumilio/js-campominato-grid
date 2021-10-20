5e000b1ebf4fa1ba64bfb4a5600ccfaf47fb3f49
lumilio <logistic-division@lumilium.com>
2021-10-20 16:05:03 +0200

    faccio in modo che al click della cella mostri numero e colore

--- asset/js/main.js
+++ asset/js/main.js
@@ -11,47 +11,51 @@ while (hardness != '1' && hardness != '2' && hardness != '3') {
 // creare una griglia di gioco diversa per ogni livello
 const griglia = document.getElementById('container')
 
+
+
 if (hardness == 1){
     for (let i = 0; i < 64; i++) {
+
+        let firstRange = Math.floor(Math.random() * 100) + 1
         let cell = document.createElement('div');
-        cell.className = 'cell_type1'
+        cell.className = 'cell_type1'      
         griglia.append(cell)
-        
-    }
+        cell.append(firstRange)  
+    } 
 }
+
+
 else if (hardness == 2){
     for (let i = 0; i < 100; i++) {
+
+        let secondRange = Math.floor(Math.random() * 81) + 1
         let cell = document.createElement('div');
         cell.className = 'cell_type2'
         griglia.append(cell)
+        cell.append(secondRange)
         
     }
 }
+
+
 else if (hardness == 3){
     for (let i = 0; i < 400; i++) {
+
+        let thirdRange = Math.floor(Math.random() * 49) + 1
         let cell = document.createElement('div');
         cell.className = 'cell_type3'
         griglia.append(cell)
+        cell.append(thirdRange)
         
     }
 }
 
-// range numeri casuali
-
-function range1() {
-    let x = Math.floor(Math.random() * 100) + 1
-}
-function range2() {
-    let x = Math.floor(Math.random() * 81) + 1
-}
-function range3() {
-    let x = Math.floor(Math.random() * 49) + 1
-}
 
 
-let firstRange = range1();
-let secondRange = range2();
-let thirdRange = range3();
+
+
+
+
 
 
 
